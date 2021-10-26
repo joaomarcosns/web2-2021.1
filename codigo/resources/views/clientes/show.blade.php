739bb78827798d0832fd2b4771d659c4dbf83ac9
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2021-10-25 21:40:53 -0300

    AtividadeIV

--- codigo/resources/views/clientes/show.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Detalhes do cliente {{ $cliente->nome }}</h1>
-
-<ul>
-    <li>NOME: {{ $cliente->nome }}</li>
-    <li>ENDEREÇO: {{ $cliente->endereco }}</li>
-    <li>DÉBITO: {{ $cliente->debito }}</li>
-</ul>

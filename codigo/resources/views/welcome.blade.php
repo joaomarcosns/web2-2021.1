12a1d1c1c941840d99ac48630f85ab35709b6cc4
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2021-10-25 21:44:37 -0300

    AtividadeIV

---
+++ codigo/resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid"> <button class="navbar-toggler navbar-toggler-right border-0 p-0" type="button"
+                data-toggle="collapse" data-target="#navbar20">
+                <p class="navbar-brand text-white mb-0"> <i class="fa d-inline fa-lg fa-stop-circle"></i> BBBOOTSTRAP </p>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar20">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"> <a class="nav-link" href="clientes/">CLIENTES</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="fornecedores/">FORNECEDOR</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="produtos/">PRODUTOS</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="entradas/">ENTRADAS</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="vendas/">VENDAS</a> </li>
+                </ul>
+                <p class="d-none d-md-block lead mb-0 text-white"> <i class="fa d-inline fa-lg fa-stop-circle"></i> <b>
+                        BBBOOTSTRAP</b> </p>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item mx-1"> <a class="nav-link" href="#"> <i
+                                class="fa fa-github fa-fw fa-lg"></i> </a> </li>
+                    <li class="nav-item mx-1"> <a class="nav-link" href="#"> <i
+                                class="fa fa-dropbox fa-fw fa-lg"></i> </a> </li>
+                    <li class="nav-item mx-1"> <a class="nav-link" href="#"> <i
+                                class="fa fa-bitbucket fa-fw fa-lg"></i> </a> </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
